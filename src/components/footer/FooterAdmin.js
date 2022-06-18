855ecdf8493e035d190f11eefcbeac9086b805cf
App Generator <51070104+app-generator@users.noreply.github.com>
2022-06-18 10:08:42 +0300

    Footers - Update Links

--- src/components/footer/FooterAdmin.js
+++ src/components/footer/FooterAdmin.js
@@ -38,18 +38,37 @@ export default function Footer() {
         {" "}
         &copy; {1900 + new Date().getYear()}
         <Text as='span' fontWeight='500' ms='4px'>
-          Horizon UI. All Rights Reserved. Made with love by
           <Link
             mx='3px'
             color={textColor}
             href='https://www.simmmple.com'
-            target='_blank'
-            fontWeight='700'>
-            Simmmple!
-          </Link>
+            target='_blank'>
+            Simmmple
+          </Link> 
+          - Coded by 
+          <Link
+            mx='3px'
+            color={textColor}
+            href='https://appseed.us'
+            target='_blank'>
+            AppSeed
+          </Link> 
         </Text>
       </Text>
       <List display='flex'>
+      <ListItem
+          me={{
+            base: "20px",
+            md: "44px",
+          }}>
+          <Link
+            fontWeight='500'
+            color={textColor}
+            target='_blank'
+            href='https://github.com/app-generator/react-horizon-ui-chakra'>
+            Source Code
+          </Link>
+        </ListItem>        
         <ListItem
           me={{
             base: "20px",
@@ -58,40 +77,9 @@ export default function Footer() {
           <Link
             fontWeight='500'
             color={textColor}
-            href='mailto:hello@simmmple.com'>
+            target='_blank'
+            href='https://appseed.us/support/'>
             Support
-          </Link>
-        </ListItem>
-        <ListItem
-          me={{
-            base: "20px",
-            md: "44px",
-          }}>
-          <Link
-            fontWeight='500'
-            color={textColor}
-            href='https://www.simmmple.com/licenses'>
-            License
-          </Link>
-        </ListItem>
-        <ListItem
-          me={{
-            base: "20px",
-            md: "44px",
-          }}>
-          <Link
-            fontWeight='500'
-            color={textColor}
-            href='https://simmmple.com/terms-of-service'>
-            Terms of Use
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link
-            fontWeight='500'
-            color={textColor}
-            href='https://www.blog.simmmple.com/'>
-            Blog
           </Link>
         </ListItem>
       </List>
--- src/components/footer/FooterAuth.js
+++ src/components/footer/FooterAuth.js
@@ -36,7 +36,6 @@ export default function Footer() {
         {" "}
         &copy; {1900 + new Date().getYear()}
         <Text as='span' fontWeight='500' ms='4px'>
-          Horizon UI. All Rights Reserved. Made with love by
           <Link
             mx='3px'
             color={textColor}
@@ -45,6 +44,14 @@ export default function Footer() {
             fontWeight='700'>
             Simmmple!
           </Link>
+          - Coded by 
+          <Link
+            mx='3px'
+            color={textColor}
+            href='https://appseed.us'
+            target='_blank'>
+            AppSeed
+          </Link>           
         </Text>
       </Text>
       <List display='flex'>
@@ -56,7 +63,20 @@ export default function Footer() {
           <Link
             fontWeight='500'
             color={linkColor}
-            href='mailto:hello@simmmple.com'>
+            target='_blank'
+            href='https://github.com/app-generator/react-horizon-ui-chakra'>
+            Source Code
+          </Link>
+        </ListItem>
+        <ListItem
+          me={{
+            base: "20px",
+            md: "44px",
+          }}>
+          <Link
+            fontWeight='500'
+            color={linkColor}
+            href='https://appseed.us/support/'>
             Support
           </Link>
         </ListItem>
@@ -68,28 +88,16 @@ export default function Footer() {
           <Link
             fontWeight='500'
             color={linkColor}
-            href='https://www.simmmple.com/licenses'>
-            License
-          </Link>
-        </ListItem>
-        <ListItem
-          me={{
-            base: "20px",
-            md: "44px",
-          }}>
-          <Link
-            fontWeight='500'
-            color={linkColor}
-            href='https://simmmple.com/terms-of-service'>
-            Terms of Use
+            href='https://simmmple.com/'>
+            Simmmple
           </Link>
         </ListItem>
         <ListItem>
           <Link
             fontWeight='500'
             color={linkColor}
-            href='https://www.blog.simmmple.com/'>
-            Blog
+            href='https://appseed.us'>
+            AppSeed
           </Link>
         </ListItem>
       </List>
